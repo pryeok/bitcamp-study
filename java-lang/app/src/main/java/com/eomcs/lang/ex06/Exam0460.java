00d63c03361da69b51711caf68c7ce71cced52a4
Park Ryuck <pryeok@gmail.com>
2021-12-12 13:37:06 +0900

    12.12

--- java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0460.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0460.java
@@ -6,11 +6,14 @@ import java.util.ArrayList;
 //
 public class Exam0460 {
   public static void main(String[] args) {
+
+
     // 배열은 같은 타입의 값만 보관할 수 있지만
     // 컬렉션은 다른 타입의 값도 보관할 수 있다.
     //
     // 배열은 크기가 고정되지만,
     // 컬렉션은 크기가 가변적이다.
+
     ArrayList list = new ArrayList();
     list.add("홍길동");
     list.add(3.14f);
@@ -21,5 +24,37 @@ public class Exam0460 {
       System.out.println(list.get(i));
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0461.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0461.java
@@ -14,9 +14,43 @@ public class Exam0461 {
 
     // for 문에 컬렉션도 적용할 수 있다.
     // for (변수 선언 : 컬렉션객체) 문장;
+
     for (Object value : list)
       System.out.println(value);
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
