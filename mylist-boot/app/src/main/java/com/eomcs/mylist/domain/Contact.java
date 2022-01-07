e97ba1968a2ab13d36a5460d8f86339582f6541e
Park Ryuck <pryeok@gmail.com>
2022-01-07 21:48:56 +0900

    01.07

--- mylist-boot/app/src/main/java/com/eomcs/mylist/domain/Contact.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/domain/Contact.java
@@ -14,6 +14,7 @@ public class Contact {
   // => 인스턴스 메서드: 특정 인스턴스 사용한다면 인스턴스 메서드로 만들라! 
   // => GRASP의 Information Expert 패턴
   //    데이터를 가공하는 기능은 그 데이터를 갖고 있는 클래스에 둬야 한다   
+
   public String toCsvString() {
     return String.format("%s,%s,%s,%s",
         this.getName(),
