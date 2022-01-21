64290381b5ae3c5825a81197b567a87b9cc8d5cf
Park Ryuck <pryeok@gmail.com>
2022-01-21 21:24:59 +0900

    01.21

--- java-lang/app/src/main/java/com/eomcs/io/ex10/Exam0210.java
+++ java-lang/app/src/main/java/com/eomcs/io/ex10/Exam0210.java
@@ -10,7 +10,9 @@ public class Exam0210 {
   public static void main(String[] args) throws Exception {
 
     FileInputStream fileIn = new FileInputStream("temp/members.data");
+
     BufferedInputStream bufIn = new BufferedInputStream(fileIn);
+
     DataInputStream in = new DataInputStream(bufIn);
 
     Member member = new Member();
