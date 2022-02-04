1c0d8e0479bb17eec5a3fcca6f27f4ad50834bcb
Park Ryuck <pryeok@gmail.com>
2022-02-04 15:58:06 +0900

    02.04

--- java-lang/app/src/main/java/com/eomcs/exception/ex2/Calculator3.java
+++ java-lang/app/src/main/java/com/eomcs/exception/ex2/Calculator3.java
@@ -13,6 +13,7 @@ public class Calculator3 {
       default:
         // 유효하지 않은 연산자인 경우 throw 명령을 이용하여 호출자에게 
         // 오류 상황을 알린다.
+        // throw 던진 명령을 catch 가 잡아서 알려준다
         throw new RuntimeException("해당 연산자를 지원하지 않습니다.");
     }
   }
