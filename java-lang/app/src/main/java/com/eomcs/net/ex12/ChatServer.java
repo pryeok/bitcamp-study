a48bab12fff352b3003bc4a79901a5a412bc028d
Park Ryuck <pryeok@gmail.com>
2022-02-10 15:01:30 +0900

    02.10

--- java-lang/app/src/main/java/com/eomcs/net/ex12/ChatServer.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex12/ChatServer.java
@@ -4,14 +4,13 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import com.eomcs.design_pattern.iterator.ArrayList;
+import java.util.ArrayList;
 
+@SuppressWarnings("rawtypes")
 public class ChatServer {
 
   int port;
-
-  @SuppressWarnings("rawtypes")
-  ArrayList clientOutStreams = new ArrayList();
+  ArrayList clientOutputStreams = new ArrayList();
 
   public ChatServer(int port) {
     this.port = port;
@@ -30,6 +29,25 @@ public class ChatServer {
     }
   }
 
+  @SuppressWarnings("unchecked")
+  public void sendMessage(String message) {
+    ArrayList deleteStreams = new ArrayList();
+
+    for (int i = 0; i < clientOutputStreams.size(); i++) {
+      DataOutputStream out = (DataOutputStream) clientOutputStreams.get(i);
+      try {
+        out.writeUTF(message);
+      } catch (Exception e) {
+        System.out.println("전송 오류: " + message);
+        deleteStreams.add(out); // 무효한 출력 스트림은 삭제 명단에 등록한다.
+      }
+    }
+
+    for (Object deleteStream : deleteStreams) { // 삭제 명단에 등록된 출력 스트림을 클라이언트 목록에서 제거한다.
+      clientOutputStreams.remove(deleteStream);
+    }
+  }
+
   class RequestHandler extends Thread {
     Socket socket;
 
@@ -37,31 +55,35 @@ public class ChatServer {
       this.socket = socket;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void run() {
       try (Socket socket2 = socket;
           DataOutputStream out = new DataOutputStream(socket.getOutputStream());
           DataInputStream in = new DataInputStream(socket.getInputStream())) {
 
-        out.writeUTF("환영합니다!");
+        clientOutputStreams.add(out);
+
+        String nickname = in.readUTF();
+
+        out.writeUTF(nickname + " 님 환영합니다!");
         out.flush();
 
         while (true) {
           String message = in.readUTF();
           if (message.equals("\\quit")) {
-            out.writeUTF("Goodbye!");
+            out.writeUTF("<![QUIT[]]>"); // 연결을 끊겠다는 특별한 메시지를 클라이언트에게 보낸다.
             out.flush();
+            clientOutputStreams.remove(out); // 메시지 출력 목록에서 연결이 종료된 클라이언트를 제거한다.
             break;
           }
-          out.writeUTF(message);
-          out.flush();
+          sendMessage(String.format("[%s] %s", nickname, message));
         }
       } catch (Exception e) {
         System.out.println("클라이언트와의 통신 오류! - " + e.getMessage());
       }
     }
   }
-
 
   public static void main(String[] args) {
     new ChatServer(8888).service();
