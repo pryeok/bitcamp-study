03e56360a05318eda8ed77f4d7319727114314b7
Park Ryuck <pryeok@gmail.com>
2022-02-11 20:15:46 +0900

    02.11

--- java-lang/app/src/main/java/com/eomcs/oop/ex11/c/Exam0110.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex11/c/Exam0110.java
@@ -16,6 +16,8 @@ class A {
     //    }
   }
 
+  static class Y {}
+
 }
 
 public class Exam0110 {
@@ -30,14 +32,14 @@ public class Exam0110 {
     obj2 = new A.Y();
 
     //1) 바깥 클래스의 인스턴스 준비
-    A  outer = null;//new A();
+    A  outer = new A();
 
     //2) 바깥 클래스의 인스턴스 주소를 사용하여 inner class의 인스턴스 생성
-    obj = outer.new X(); // => 컴파일러는 다음 코드로 변경한다. new A.X(outer)
+    obj = outer.new X();
 
-    // 컴파일러는 inner 클래스의 객체를 만드는 코드를 컴파일 할 때 다음과 같이 
+    // 컴파일러는 컴파일 할 때 다음과 같이 
     // - 바깥 클래스의 객체를 생성자에 전달하는 코드로 변경한다.
-    //    obj = new X(outer);
+    //    obj = new A.X(outer);
 
   }
 
