adf42c49617eda7677f6a944e49ace132ba5d27c
Park Ryuck <pryeok@gmail.com>
2022-02-24 18:39:14 +0900

    02.24

--- java-lang/app/src/main/java/com/eomcs/oop/ex10/b/Exam01.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex10/b/Exam01.java
@@ -4,9 +4,15 @@ package com.eomcs.oop.ex10.b;
 public class Exam01 implements Servlet {
   // 인터페이스를 구현한다는 것은 
   // 인터페이스에 선언된 모든 메서드를 구현한다는 뜻이다!
+  @Override
   public void init() {}
+  @Override
   public void service() {}
+  @Override
   public void destroy() {}
+  @Override
   public String getServletInfo() {return null;}
+  @Override
   public String getServletConfig() {return null;}
+
 }
