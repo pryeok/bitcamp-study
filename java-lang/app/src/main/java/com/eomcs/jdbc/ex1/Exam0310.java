f0f0638903fa10a9312cdd05f54f73b8299c5d97
Park Ryuck <pryeok@gmail.com>
2022-03-04 14:03:26 +0900

    03.04

--- java-lang/app/src/main/java/com/eomcs/jdbc/ex1/Exam0310.java
+++ java-lang/app/src/main/java/com/eomcs/jdbc/ex1/Exam0310.java
@@ -8,7 +8,7 @@ public class Exam0310 {
   public static void main(String[] args) throws Exception {
 
     try (java.sql.Connection con = DriverManager.getConnection(
-        "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
+        "jdbc:mariadb://localhost:3306/studydb?user=study&password=1111");
 
         // java.sql.Statement 구현 객체를 얻는다.
         // - SQL문을 DBMS의 형식에 따라 인코딩하여 서버에 전달하는 일을 하는 객체.
@@ -25,7 +25,7 @@ public class Exam0310 {
       //
       // 2) SELECT 등 DQL 관련 SQL문 전송
       // => executeQuery()
-      // => 리턴값: 서버에서 데이터를 가져오는 일을 할 객체
+      // => 리턴값: 서버에서 데이터를 가져오는 일을 할 객체!!!
       //
       // 용어정리
       // "DML(Data Manipulation Language)"
