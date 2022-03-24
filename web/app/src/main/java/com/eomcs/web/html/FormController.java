1cec780a03f044cf2a055692280a9f3e6aea9cd4
Park Ryuck <pryeok@gmail.com>
2022-03-24 17:16:49 +0900

    myPage

--- web/app/src/main/java/com/eomcs/web/html/FormController.java
+++ web/app/src/main/java/com/eomcs/web/html/FormController.java
@@ -60,4 +60,35 @@ public class FormController {
     }
     return "ok!"; 
   }
+
+  @RequestMapping("/html/form/exam21")
+  public Object exam21(String name, int age) throws Exception {
+    System.out.println(name);
+    System.out.println(age);
+
+    // Thread.sleep(10000);
+
+    return "ok!"; 
+  }
+
+  @RequestMapping("/html/form/exam31")
+  public Object exam31(String name, int age, MultipartFile photo) {
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(photo.getOriginalFilename());
+
+    if (photo != null) {
+      System.out.println(photo.getOriginalFilename());
+    }
+
+    //    try {
+    //      File photoFile = new File("c:/upload/" + photo.getOriginalFilename());
+    //      photo.transferTo(photoFile);
+    //    } catch (Exception e) {
+    //      return "error!";
+    //    }
+
+    return "ok!"; 
+  }
+
 }
