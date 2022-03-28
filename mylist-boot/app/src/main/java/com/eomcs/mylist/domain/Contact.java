6ce6c0290a8580b12ec87faa17acc7fa6d656980
Park Ryuck <pryeok@gmail.com>
2022-03-28 16:55:35 +0900

    lombok

--- mylist-boot/app/src/main/java/com/eomcs/mylist/domain/Contact.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/domain/Contact.java
@@ -1,7 +1,9 @@
 package com.eomcs.mylist.domain;
 
 import java.util.List;
+import lombok.Data;
 
+@Data
 public class Contact {
 
   int no;
@@ -9,40 +11,4 @@ public class Contact {
   String email;
   String company;
   List<ContactTel> tels;
-
-  @Override
-  public String toString() {
-    return "Contact [no=" + no + ", name=" + name + ", email=" + email + ", company=" + company
-        + ", tels=" + tels + "]";
-  }
-  public int getNo() {
-    return no;
-  }
-  public void setNo(int no) {
-    this.no = no;
-  }
-  public String getName() {
-    return name;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
-  public String getEmail() {
-    return email;
-  }
-  public void setEmail(String email) {
-    this.email = email;
-  }
-  public String getCompany() {
-    return company;
-  }
-  public void setCompany(String company) {
-    this.company = company;
-  }
-  public List<ContactTel> getTels() {
-    return tels;
-  }
-  public void setTels(List<ContactTel> tels) {
-    this.tels = tels;
-  }
 }
